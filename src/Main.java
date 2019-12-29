649661bb0402e9279b05640d991fa05285940b37
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2019-12-29 14:34:27 -0500

    Reformat main.java

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         //Reads and prints Mouse.txt
-        File a =new File("src/Happy.txt");
-        Scanner scan=new Scanner(a);
-        while(scan.hasNextLine())
+        File a = new File("src/Happy.txt");
+        Scanner scan = new Scanner(a);
+        while (scan.hasNextLine())
             System.out.println(scan.nextLine());
         System.out.println();
     }
